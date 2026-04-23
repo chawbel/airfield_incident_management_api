22f40b19030bf734fa730d336eb69f270d07914f
charbel <charbell.bahry@gmail.com>
2026-04-23 16:33:59 +0000

    feat(api): add schema for detection response

--- app/routers/detection_router.py
+++ app/routers/detection_router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.schemas.detection_schema import DetectionCreate
+from app.schemas.detection_schema import DetectionCreate, DetectionResponse
 from app.services.detection_service import process_detection
 from sqlalchemy.orm import Session
 from app.core.dependencies import get_db
@@ -8,6 +8,6 @@ from app.core.dependencies import get_db
 router = APIRouter(prefix="/detection", tags=["Detections"])
 
 
-@router.post("/")
+@router.post("/", response_model=DetectionResponse)
 def create_detection(detection: DetectionCreate, db: Session = Depends(get_db)):
     return process_detection(db, detection)
--- app/schemas/detection_schema.py
+++ app/schemas/detection_schema.py
@@ -1,7 +1,17 @@
 from pydantic import BaseModel
+from app.models.enums import IncidentStatus
 
 
 class DetectionCreate(BaseModel):
     camera_id: int
     object_type: str
     confidence: float
+
+
+class DetectionResponse(BaseModel):
+    detection_id: int
+    camera_id: int
+    object_type: str
+    confidence: float
+    incident_id: int
+    status: IncidentStatus
