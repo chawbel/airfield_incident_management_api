46b60d4a7efe4521de5036c038a89554e6a68e9a
charbel <charbell.bahry@gmail.com>
2026-04-23 16:19:42 +0000

    feat(detection): integrate database and trigger incident creation

--- app/routers/detection_router.py
+++ app/routers/detection_router.py
@@ -1,12 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from app.schemas.detection_schema import DetectionCreate
 from app.services.detection_service import process_detection
+from sqlalchemy.orm import Session
+from app.core.dependencies import get_db
 
 
 router = APIRouter(prefix="/detection", tags=["Detections"])
 
 
 @router.post("/")
-def create_detection(detection: DetectionCreate):
-    result = process_detection(detection)
-    return result
+def create_detection(detection: DetectionCreate, db: Session = Depends(get_db)):
+    return process_detection(db, detection)
--- app/services/detection_service.py
+++ app/services/detection_service.py
@@ -1,10 +1,19 @@
 from app.schemas.detection_schema import DetectionCreate
+from sqlalchemy.orm import Session
+from app.repositories.detection_repository import create_detection
+from app.repositories.incident_repository import create_incident
 
 
-def process_detection(detection: DetectionCreate):
+def process_detection(db: Session, detection: DetectionCreate):
+    db_detection = create_detection(db, detection)
+
+    incident = create_incident(db, db_detection.id)
+
     return {
-        "camera_id": detection.camera_id,
-        "object_type": detection.object_type,
-        "confidence": detection.confidence,
-        "incident_created": True,
+        "detection_id": db_detection.id,
+        "camera_id": db_detection.camera_id,
+        "object_type": db_detection.object_type,
+        "confidence": db_detection.confidence,
+        "incident_id": incident.id,
+        "status": incident.status,
     }
